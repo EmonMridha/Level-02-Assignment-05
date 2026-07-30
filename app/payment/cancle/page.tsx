01cabf3bfffe997a0c8e5f13d2f0336a4e63c407
EmonMridha <emonmridha712@gmail.com>
2026-07-30 17:12:02 +0600

    first commit

---
+++ app/payment/cancle/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const canclePage = () => {
+  return (
+    <div>canclePage</div>
+  )
+}
+
+export default canclePage
---
+++ app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const successPage = () => {
+  return (
+    <div>successPage</div>
+  )
+}
+
+export default successPage
